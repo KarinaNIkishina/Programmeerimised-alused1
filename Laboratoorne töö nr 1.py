7238dfc0d056a0c9d2abe7478e84a031eec51738
Karina Nikishina <nickkari4ik@gmail.com>
2025-09-10 11:10:20 +0300

    Laboratoorne töö nr 1

---
+++ Laboratoorne töö nr 1.py
@@ -0,0 +1,31 @@
+print("ülesanne 1")
+
+täisarv1 = int(input("sisesta esimene täisarv: "))
+täisarv2 = int(input("sisesta teine täisarv: "))
+summa = (täisarv1 + täisarv2)
+vahe = (täisarv1 - täisarv2)
+korrutis = (täisarv1 * täisarv2)
+print ("summa:", summa)
+print ("vahe:", vahe)
+print ("korrutis:", korrutis)
+
+print("ülesanne 2")
+
+nimi = input("Sisesta oma nimi:")
+vanus = int(input("Sisesta oma vanus:"))
+sünniaasta = (2025 - vanus)
+print ("Sisesta oma nimi:", nimi)
+print ("Sisesta oma vanus:", vanus)
+print ("Tere!", nimi)
+print ("Sinu sünniaasta on ilmselt", sünniaasta)
+
+print("ülesanne 4")
+
+arv1 = int(input("sisesta esimene arv (a): "))
+arv2 = int(input("sisesta teine arv (b): "))
+print("enne vahetamist: a =", arv1, "b =", arv2)
+print("peale vahetamist: b =", arv1, "a =", arv2)
+
+
+
+
