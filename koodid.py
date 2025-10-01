81f9bdb0f107d419f85c266de425de87f1b4b892
Karina Nikishina <nickkari4ik@gmail.com>
2025-10-01 11:35:09 +0300

    Add files via upload

---
+++ koodid.py
@@ -0,0 +1,86 @@
+#userInput = int(input("sisesta arv: "))
+#i = 1
+#while i<= userInput:
+    #print("hello world" , "trükl töötas nii palju korda", i)
+    #i = i + 1
+    
+    
+#userInput = int (input ("sisesta arv:"))
+#while userInput >= 1:
+    #print("hello world", "trükl töötas nii palju korda",userInput)
+    #userInput = userInput - 1
+
+
+
+#userInput = int(input("sisesta arv:"))
+#i = 1
+#numSum = 0      
+#while i<= userInput:
+    #numSum = numSum + i
+    #print("sum: ", numSum)
+    #i = i + 1
+     
+#print("sum: ", numSum)
+
+#userInput = int(input("Sisesta arv: "))
+#i = 1
+#oddNumber = 0
+#evenNumber = 0
+#while i <= userInput:
+    #if i % 2 == 1:
+        #print(i, "paritu arv")
+        #oddNumber = oddNumber + 1
+    #else:
+        #print(i, "paris arv")
+        #evenNumber+=1
+    #i = i + 1
+#print("paaritute arvude arv: ", oddNumber, "paaris arvude arv:", evenNumber)
+    
+
+#break
+#k = 0
+#while k<5:
+    #print("k: ", k)
+    #k+=1
+    #if(k == 3):
+        #print("töörab break")
+        #break
+ 
+#num = 33
+#count = 3
+#while True:
+    #userInput = int(input("sisesta arv: "))
+    #if userInput < num:
+        #print("sa sisestan vähem arv kui oli planeeritud")
+    #elif userInput == num:
+        #print("sa valisid õige arv")
+        #break
+    #else:
+        #print("sa sisestad suurem arv kui oli planeeritud")
+        #count -= 1
+
+
+
+
+vali card pinkood = "1234"
+blocked = False
+balance = 100.00
+
+
+card code = input("Sisestage kaardi PIN-kood")
+if card code != vali card pinkood:
+    print("Vale PIN-kood. Kaart on blokeeritud. ")
+    blocked = True
+    
+    
+if not blocked:
+    attempts = 3
+    while attempts > 0:
+        pin = input
+
+        
+
+
+
+
+
